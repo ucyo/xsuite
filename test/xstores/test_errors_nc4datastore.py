a06e31efe55b1c57ce8218ae669cca8d75b47e7e
ucyo <cayoglu@me.com>
2017-07-05 14:59:50 +0200

    Test wrong input

--- test/xstores/test_errors_nc4datastore.py
+++ test/xstores/test_errors_nc4datastore.py
@@ -16,6 +16,12 @@ def test_not_nc_dataset():
     assert 'ds is not a NETCDF4 dataset' in str(err)
 
 
+def test_not_str_or_dataset():
+    with pytest.raises(TypeError) as err:
+        xstores.NC4DataStore(32)
+    assert 'Object is neither a string nor a NETCDF4' in str(err)
+
+
 def test_without_format():
     data = nc4.Dataset(filename)
     ds = xr.Dataset.load_store(xstores.NC4DataStore(data, format=None))
