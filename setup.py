1853bccbfdd525808650570a073358ab7e83b64c
ucyo <cayoglu@me.com>
2017-08-02 17:38:13 +0200

    Move versioning to xsuite/__init__.py

--- setup.py
+++ setup.py
@@ -3,15 +3,24 @@
 """Minimal setup."""
 
 from setuptools import setup, find_packages
+import os
+import re
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-version = '0.2.5'
 
-setup(name='xsuite',
+def get_property(prop, project):
+    with open(os.path.join(project, '__init__.py')) as f:
+        result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                           f.read())
+    return result.group(1)
+
+
+project = 'xsuite'
+setup(name=project,
       install_requires=requirements,
-      version=version,
+      version=get_property('__version__', project),
       description='Several extensions for xarray',
       author='Ugur Cayoglu',
       author_email='urcyglu@gmail.com',
