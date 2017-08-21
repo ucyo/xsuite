087a04f35beda97723a8f065544228a949929a2b
ucyo <cayoglu@me.com>
2017-08-21 15:50:10 +0200

    Change environmental variable name

--- tests/xtend/test_environmental_vars.py
+++ tests/xtend/test_environmental_vars.py
@@ -2,19 +2,18 @@
 # coding=utf-8
 """Test environmental variables for extension of xarray."""
 
-import xarray as xr
 import os
 from xsuite.tools import load_data
 
-DA_ENV = 'XSUITE_DA_FOLDERS'
-DS_ENV = 'XSUITE_DS_FOLDERS'
+
+DA_ENV = 'XSUITE_DA_METHODS'
+DS_ENV = 'XSUITE_DS_METHODS'
+
 
 for var in [DA_ENV, DS_ENV]:
-    if not os.getenv(var, False):
-        os.environ[var] = 'xtend/da_methods:xtend/da_methods_other' if 'DA' in var else 'xtend/ds_methods'
+    os.environ[var] = 'xtend/da_methods:xtend/da_methods_other' if 'DA' in var else 'xtend/ds_methods'
 
-FILENAME = 'data/sresa1b_ncar_ccsm3-example.nc'
-DS = xr.open_dataset(FILENAME, decode_times=False)
+
 FILENAME = 'sresa1b_ncar_ccsm3-example.nc'
 DS = load_data(FILENAME, decode_times=False)
 
@@ -27,12 +26,13 @@ def test_load_from_env():
 
 
 def test_load_from_env_and_folder():
-    os.environ[DA_ENV] = 'xtend/da_methods_another'
+    os.environ[DA_ENV] = os.path.join(os.path.dirname(__file__), 'da_methods_another')
     from xsuite import xtend
-    DS.tas.xtend.load_env()
+    DS.tas.xtend._load_env()
     assert hasattr(DS.tas.xtend, 'climate')
 
 
 def test_reload_importing():
     from xsuite import xtend
     from xsuite import xtend
+    assert True  # Reimport worked
