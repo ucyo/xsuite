c4a5af03c0f5a1d4d32fdbde9b4edcaae88880e1
ucyo <cayoglu@me.com>
2017-09-27 22:36:37 +0200

    Add test for python without 'main'

--- tests/xtend/test_errors_xtend.py
+++ tests/xtend/test_errors_xtend.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 """Tests for importing folders with extensions."""
 
+import os
 import pytest
 from xsuite import xtend
 from xsuite.tools import load_data
@@ -23,3 +24,10 @@ def test_mode_err():
 
 def test_no_folder():
     assert not xtend.xtend_dataarray()
+
+def test_no_main():
+    with pytest.raises(AttributeError) as err:
+        folder, _ = os.path.split(os.path.abspath(__file__))
+        xtend.extend._add_folder(os.path.join(folder, 'false_da_methods'), mode='da')
+        getattr(DS.tas.xtend, 'false_declaration_anomalies')
+    assert "no attribute" in str(err)
